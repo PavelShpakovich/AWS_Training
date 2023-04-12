b43f41d904574e25fa455c9c441ac592548e3adf
Pavel Shpakovich <pavel.shpakovich@inspirebrands.com>
2023-04-12 15:42:02 +0300

    Adding correct import endpoint and ability to upload files

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://rvlore6hjf.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://x8ho3fk4xj.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
