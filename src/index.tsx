6ca4e522fefd6d41059b015f90877a58a01061af
Pavel Shpakovich <pavel.shpakovich@inspirebrands.com>
2023-05-01 22:18:51 +0300

    [TASK-7]

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,6 +21,21 @@ if (import.meta.env.DEV) {
   worker.start({ onUnhandledRequest: "bypass" });
 }
 
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    switch (error.response.status) {
+      case 401:
+        toast.error(`Something went wrong during request: Unauthorized`);
+      case 403:
+        toast.error(`Something went wrong during request: Access denied`);
+      default:
+        toast.error(`Something went wrong during request: ${error.message}`);
+    }
+    return error;
+  }
+);
+
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
@@ -28,6 +45,12 @@ root.render(
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
+          <Toaster
+            position="top-center"
+            toastOptions={{
+              duration: 2000,
+            }}
+          />
           <App />
         </ThemeProvider>
         <ReactQueryDevtools initialIsOpen={false} />
